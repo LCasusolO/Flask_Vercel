448d9de295393e4a06d68495ae2490c288f3b022
ELKsu <luis.casusol.ortega@gmail.com>
2024-04-12 16:02:17 -0500

    Flask y Vercel

--- app.py
+++ app.py
@@ -21,17 +21,28 @@ def calcular_muestra():
         K=1.96
         #p = float(request.form['p'])
         p=0.25
-        N = int(request.form['N'])
+        N1 = int(request.form['N1'])
+        N2 = int(request.form['N2'])
+        N3 = int(request.form['N3'])
+
         #E = float(request.form['E'])
         E = 0.05
-        n = (K**2 * N * p * (1-p)) / (((N-1) * E**2) + (K**2 * p * (1-p)))
-        n = math.ceil(n)  # Redondear hacia arriba para obtener un entero
+        n1 = (K**2 * N1 * p * (1-p)) / (((N1-1) * E**2) + (K**2 * p * (1-p)))
+        n1 = math.ceil(n1)  # Redondear hacia arriba para obtener un entero
+
+        n2 = (K**2 * N2 * p * (1-p)) / (((N2-1) * E**2) + (K**2 * p * (1-p)))
+        n2 = math.ceil(n2)  # Redondear hacia arriba para obtener un entero
+
+        n3 = (K**2 * N3 * p * (1-p)) / (((N3-1) * E**2) + (K**2 * p * (1-p)))
+        n3 = math.ceil(n3)  # Redondear hacia arriba para obtener un entero
+
+        n = n1 + n2 + n3
         resultado_visible = True  # Hacemos visible el resultado después de calcular
         # Retorna las variables incluyendo resultado_visible
-        return render_template('muestra.html', resultado_visible=resultado_visible, n=n, K=K, p=p, N=N, E=E)
+        return render_template('muestra.html', resultado_visible=resultado_visible, n1=n1, n2=n2, n3=n3, n=n, K=K, p=p, N1=N1, N2=N2, N3=N3, E=E)
     else:
         # La página se carga por primera vez, el resultado no debe ser visible
-        return render_template('muestra.html', resultado_visible=resultado_visible, n=None, K=None, p=None, N=None, E=None)
+        return render_template('muestra.html', resultado_visible=resultado_visible, n1=None, n2=None, n3=None, n=None, K=None, p=None, N1=None, N2=None, N3=None, E=None)
     
 # Ejecutar la aplicación
 if __name__ == '__main__':
